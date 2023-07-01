94a566cbcfccac051825d9b96513769d9da8c546
khushi <khushisuri9252gmail.com>
2023-07-01 20:29:10 +0530

    Kushi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import StudentRegister from "./pages/StudentRegister";
 import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
@@ -9,7 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/' element={<StudentRegister />} />
+          <Route path='/str' element={<StudentRegister />} />
         </Routes>
 
       </BrowserRouter>
